c5f5d7fb181fa55eba2bf9e250245586a60eed7e
Glahatte <55100132+Glahatte@users.noreply.github.com>
2019-11-04 15:35:15 -0500

    Add files via upload

---
+++ FastfoodApp.js
@@ -0,0 +1,24 @@
+var customerOrder= document.getElementById(customerOrder)
+
+function GetfoodOrder(){
+    var input= document.getElementById(Entree + (sideDish)).value;
+alert(input);
+
+}
+document.getElementById("customerOrder").addEventListener("click");
+
+
+var menu=[
+{"burger",inventory1: '30'},
+ {"fries",inventory2: 50},
+ {"hotdog", inventory 3:45},
+ {"salad",inventory 4:20} ];
+
+var i = menu
+var text ="";
+for (i=0; i <menu.length; i++){
+    text += menu[i] + "<br>";
+}
+
+let person =[{order}]
+
